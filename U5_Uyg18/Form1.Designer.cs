7559e4b8aeeeca3d227add9c9003b7c071217402
BIL-LAB1PC14\Ogrenci <123@123.COM>
2023-04-11 15:23:19 +0300

    no message

---
+++ U5_Uyg18/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace U5_Uyg18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.maskTC = new System.Windows.Forms.MaskedTextBox();
+            this.maskTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.maskDTarih = new System.Windows.Forms.MaskedTextBox();
+            this.maskKart = new System.Windows.Forms.MaskedTextBox();
+            this.maskİp = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TC.No :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Telefon :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Doğum Tarihi :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Kart Numarası :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "IP Adresi :";
+            // 
+            // maskTC
+            // 
+            this.maskTC.Location = new System.Drawing.Point(86, 22);
+            this.maskTC.Name = "maskTC";
+            this.maskTC.Size = new System.Drawing.Size(100, 20);
+            this.maskTC.TabIndex = 11;
+            // 
+            // maskTelefon
+            // 
+            this.maskTelefon.Location = new System.Drawing.Point(86, 48);
+            this.maskTelefon.Name = "maskTelefon";
+            this.maskTelefon.Size = new System.Drawing.Size(100, 20);
+            this.maskTelefon.TabIndex = 12;
+            // 
+            // maskDTarih
+            // 
+            this.maskDTarih.Location = new System.Drawing.Point(86, 74);
+            this.maskDTarih.Name = "maskDTarih";
+            this.maskDTarih.Size = new System.Drawing.Size(100, 20);
+            this.maskDTarih.TabIndex = 13;
+            // 
+            // maskKart
+            // 
+            this.maskKart.Location = new System.Drawing.Point(86, 100);
+            this.maskKart.Name = "maskKart";
+            this.maskKart.Size = new System.Drawing.Size(100, 20);
+            this.maskKart.TabIndex = 14;
+            // 
+            // maskİp
+            // 
+            this.maskİp.Location = new System.Drawing.Point(86, 126);
+            this.maskİp.Name = "maskİp";
+            this.maskİp.Size = new System.Drawing.Size(100, 20);
+            this.maskİp.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 170);
+            this.Controls.Add(this.maskİp);
+            this.Controls.Add(this.maskKart);
+            this.Controls.Add(this.maskDTarih);
+            this.Controls.Add(this.maskTelefon);
+            this.Controls.Add(this.maskTC);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox maskTC;
+        private System.Windows.Forms.MaskedTextBox maskTelefon;
+        private System.Windows.Forms.MaskedTextBox maskDTarih;
+        private System.Windows.Forms.MaskedTextBox maskKart;
+        private System.Windows.Forms.MaskedTextBox maskİp;
+    }
+}
+
---
+++ U5_Uyg18/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U5_Uyg18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            maskTC.Mask = "00000000000";
+            maskTelefon.Mask = "(999) 000 00 00";
+            maskDTarih.Mask = "00/00/0000";
+            maskKart.Mask = "0000-0000-0000-0000";
+            maskİp.Mask = "###.###.###.###";
+            
+            
+        }
+    }
+}
