c49455e9b25678dcef6fcfd07462cc59f36d0566
BIL-LAB1PC13\Ogrenci <123@123.COM>
2023-04-25 11:37:47 +0300

    no message

---
+++ U5_Uyg24/Ogrenciler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U5_Uyg24
+{
+    class Ogrenciler
+    {
+        public int Numara { get; set; }
+        public string AdSoyad { get; set; }
+        public int DersNotu { get; set; }
+    }
+}
