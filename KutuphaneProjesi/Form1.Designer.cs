23a3caa886286015c6f9dbf831e3ff15f9e2c951
BIL-LAB1PC14\Ogrenci <123@123.COM>
2023-05-09 16:19:09 +0300

    no message

--- KutuphaneProjesi/Form1.Designer.cs
+++ KutuphaneProjesi/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace KutuphaneProjesi
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAnaSayfa));
             this.btnKitap = new System.Windows.Forms.Button();
             this.btnOgrenci = new System.Windows.Forms.Button();
             this.btnTur = new System.Windows.Forms.Button();
@@ -37,39 +38,51 @@ namespace KutuphaneProjesi
             // 
             // btnKitap
             // 
+            this.btnKitap.Image = ((System.Drawing.Image)(resources.GetObject("btnKitap.Image")));
             this.btnKitap.Location = new System.Drawing.Point(34, 45);
             this.btnKitap.Name = "btnKitap";
             this.btnKitap.Size = new System.Drawing.Size(170, 75);
             this.btnKitap.TabIndex = 0;
-            this.btnKitap.Text = "button1";
+            this.btnKitap.Text = "Kitap İşlemleri";
+            this.btnKitap.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnKitap.UseVisualStyleBackColor = true;
+            this.btnKitap.Click += new System.EventHandler(this.btnKitap_Click);
             // 
             // btnOgrenci
             // 
+            this.btnOgrenci.Image = ((System.Drawing.Image)(resources.GetObject("btnOgrenci.Image")));
             this.btnOgrenci.Location = new System.Drawing.Point(228, 45);
             this.btnOgrenci.Name = "btnOgrenci";
             this.btnOgrenci.Size = new System.Drawing.Size(170, 75);
             this.btnOgrenci.TabIndex = 1;
-            this.btnOgrenci.Text = "button2";
+            this.btnOgrenci.Text = "Öğrenci İşlemleri";
+            this.btnOgrenci.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnOgrenci.UseVisualStyleBackColor = true;
+            this.btnOgrenci.Click += new System.EventHandler(this.btnOgrenci_Click);
             // 
             // btnTur
             // 
+            this.btnTur.Image = ((System.Drawing.Image)(resources.GetObject("btnTur.Image")));
             this.btnTur.Location = new System.Drawing.Point(34, 136);
             this.btnTur.Name = "btnTur";
             this.btnTur.Size = new System.Drawing.Size(170, 75);
             this.btnTur.TabIndex = 2;
-            this.btnTur.Text = "button3";
+            this.btnTur.Text = "Tür İşlemleri";
+            this.btnTur.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnTur.UseVisualStyleBackColor = true;
+            this.btnTur.Click += new System.EventHandler(this.btnTur_Click);
             // 
             // btnOdunc
             // 
+            this.btnOdunc.Image = ((System.Drawing.Image)(resources.GetObject("btnOdunc.Image")));
             this.btnOdunc.Location = new System.Drawing.Point(228, 136);
             this.btnOdunc.Name = "btnOdunc";
             this.btnOdunc.Size = new System.Drawing.Size(170, 75);
             this.btnOdunc.TabIndex = 3;
-            this.btnOdunc.Text = "button4";
+            this.btnOdunc.Text = "Ödünç Kitap İşlemleri";
+            this.btnOdunc.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnOdunc.UseVisualStyleBackColor = true;
+            this.btnOdunc.Click += new System.EventHandler(this.btnOdunc_Click);
             // 
             // FormAnaSayfa
             // 
--- KutuphaneProjesi/Form1.cs
+++ KutuphaneProjesi/Form1.cs
@@ -16,5 +16,29 @@ namespace KutuphaneProjesi
         {
             InitializeComponent();
         }
+
+        private void btnKitap_Click(object sender, EventArgs e)
+        {
+            formKitap kitap = new formKitap();
+            kitap.ShowDialog();
+        }
+
+        private void btnOgrenci_Click(object sender, EventArgs e)
+        {
+            formOgrenci ogrenci = new formOgrenci();
+            ogrenci.ShowDialog();
+        }
+
+        private void btnTur_Click(object sender, EventArgs e)
+        {
+            formKitapTur kitapTur = new formKitapTur();
+            kitapTur.ShowDialog();
+        }
+
+        private void btnOdunc_Click(object sender, EventArgs e)
+        {
+            formOduncKitap oduncKitap = new formOduncKitap();
+            oduncKitap.ShowDialog();
+        }
     }
 }
