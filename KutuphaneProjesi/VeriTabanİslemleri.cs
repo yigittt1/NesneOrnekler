322f3f98903467744f9498784cc3d9198500ac71
BIL-LAB1PC14\Ogrenci <123@123.COM>
2023-05-09 15:38:33 +0300

    no message

---
+++ KutuphaneProjesi/VeriTabanİslemleri.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KutuphaneProjesi
+{
+    class VeriTabanİslemleri
+    {
+        string baglantiCumlesi = ConfigurationManager.ConnectionStrings["kutuphaneBaglantiCumlesi"].ConnectionString;
+
+        public MySqlConnection baglan()
+        {
+            MySqlConnection baglanti = new MySqlConnection(baglantiCumlesi);
+
+            MySqlConnection.ClearPool(baglanti);
+            return baglanti;
+        }
+
+
+
+
+
+
+    }
+}
