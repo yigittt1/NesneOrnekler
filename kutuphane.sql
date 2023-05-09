cb057111e20cb05c56c510ec93434bdb5a848ebf
BIL-LAB1PC14\Ogrenci <123@123.COM>
2023-05-09 11:10:08 +0300

    no message

--- kutuphane.sql
+++ kutuphane.sql
@@ -1,1 +1,39 @@
-UPDATE personel SET maas=maas+(maas*25/100) WHERE departman ="Yazılım Geliştirme"
+-- --------------------------------------------------------
+-- Sunucu:                       127.0.0.1
+-- Sunucu sürümü:                8.0.21 - MySQL Community Server - GPL
+-- Sunucu İşletim Sistemi:       Win64
+-- HeidiSQL Sürüm:               11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- kutuphane için veritabanı yapısı dökülüyor
+DROP DATABASE IF EXISTS `kutuphane`;
+CREATE DATABASE IF NOT EXISTS `kutuphane` /*!40100 DEFAULT CHARACTER SET latin5 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `kutuphane`;
+
+-- tablo yapısı dökülüyor kutuphane.kitaplar
+DROP TABLE IF EXISTS `kitaplar`;
+CREATE TABLE IF NOT EXISTS `kitaplar` (
+  `kitap_id` int NOT NULL AUTO_INCREMENT,
+  `tur_id` tinyint NOT NULL DEFAULT '0',
+  `kitap_adi` varchar(40) NOT NULL DEFAULT '0',
+  `yazar` varchar(40) NOT NULL DEFAULT '0',
+  `yayinevi` varchar(40) NOT NULL DEFAULT '0',
+  `sayfa_sayisi` smallint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`kitap_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- kutuphane.kitaplar: ~0 rows (yaklaşık) tablosu için veriler indiriliyor
+DELETE FROM `kitaplar`;
+/*!40000 ALTER TABLE `kitaplar` DISABLE KEYS */;
+/*!40000 ALTER TABLE `kitaplar` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
